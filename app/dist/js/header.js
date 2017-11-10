d7aaa071ad2ee074426dc83f226dc52c10cdcbc1
nykim <ppphlox@gmail.com>
2017-11-10 19:37:47 +0900

    트윈맥스 적용중

--- app/dist/js/header.js
+++ app/dist/js/header.js
@@ -15,9 +15,32 @@ $(function(){
     
     function menuOpen(){
         console.log('메뉴 열림!');
-        $gnb.toggleClass('open');
-        //$gnb.slideToggle();
-        console.log('..이제 gnb의 높이는, '+$(".gnb").height());
+       // $gnb.toggleClass('open');
+        
+        var $nav = $(".nav");
+        var $menu = {
+            item_1 : $nav.children().eq(0),
+            item_2 : $nav.children().eq(1),
+            item_3 : $nav.children().eq(2)
+        };
+        var $contact = $nav.next();
+        
+        var tl_nav = new TimelineLite();
+        
+        tl_nav
+            .staggerFrom( 
+                [$menu.item_1, $menu.item_2, $menu.item_3], //각 아이템 및 순서
+                0.8, //시간
+                {opacity: 0, top: 20}, //애니메이션
+                0.2 //간격
+            )
+            .addLabel("menuShow")
+            .from(
+                $contact,
+                1,
+                {opacity: 0},
+                "menuShow-=0.25"
+            );
     }
     
     
@@ -33,7 +56,6 @@ $(function(){
     $(window).on('load',function(){
         init();
         initEvent();
-        console.log('초기 gnb의 높이는, '+$(".gnb").height());
     });
     
 });
