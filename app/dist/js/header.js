89e5650558ee517c3aab132687febe836d0664fb
nykim <ppphlox@gmail.com>
2017-11-13 17:55:01 +0900

    [공통요소] 헤더 gnb 애니메이션 동작 완료

--- app/dist/js/header.js
+++ app/dist/js/header.js
@@ -3,18 +3,18 @@ $(function(){
     
     
     // 전역변수 선언
-    var $menu = null;
+    var $body = null;
+    var $burger = null;
     var $gnb = null;
     var tl_nav_show = null;
-    //var tl_nav_hide = null;
     
     // 전역에서 사용할 요소 초기화
     function init(){
-        $menu = $(".menu");
+        $body = $("body");
+        $burger = $(".menu");
         $gnb = $(".gnb");
         
         tl_nav_show = new TimelineLite({paused:true, reversed:true});
-        //tl_nav_hide = new TimelineLite({paused:true});
         
     }
     
@@ -32,29 +32,84 @@ $(function(){
             .addLabel("menuOpen")
             .staggerFrom( //네비 리스트 보이기
                 [$menu.item_1, $menu.item_2, $menu.item_3], //각 아이템 및 순서
-                0.6, //듀레이션
+                0.7, //듀레이션
                 {opacity: 0, top: 20}, //애니메이션
-                0.16, //간격
-                "menuOpen+=0.5"
+                0.2, //간격
+                "menuOpen+=0.4"
             )
             .addLabel("menuShow")
             .from( //하단 contact 정보 보이기
                 $contact, //대상
-                0.7, //듀레이션
+                0.5, //듀레이션
                 {opacity: 0}, //애니메이션
-                "menuShow-=0.15" //시간 조절
+                "menuShow-=0.32" //시간 조절
             );
         
         
     }
     
+    function openBurgerMenu(){
+            $burger.addClass('open'); //버거 아이콘 바꾸기
+            tl_nav_show.play().timeScale(1); //애니메이션 실행
+            $gnb.addClass('open'); //GNB배경 보이기
+            preventScroll();
+        
+            function preventScroll(){
+                $("html").addClass("no_scroll");
+                $(".wrap").css({"margin-right":scrollBarWidth()}); 
+                $(".footer").css({width:"calc(100% - "+scrollBarWidth()+"px)"}); 
+                $(".menu").css({right:'+='+scrollBarWidth()});
+            }
+        
+        	//브라우저별로 달라지는 스크롤바 너비 구하는 함수 (javascript)
+            function scrollBarWidth() {
+                document.body.style.overflow = 'hidden'; 
+                var width = document.body.clientWidth;
+
+                document.body.style.overflow = 'scroll'; 
+                width -= document.body.clientWidth; 
+
+                if(!width) width = document.body.offsetWidth - document.body.clientWidth;
+
+                document.body.style.overflow = ''; 
+
+                return width; 
+            }
+
+        
+        
+    }
+    
+    function closeBurgerMenu(){
+           $burger.removeClass('open'); //버거 아이콘 바꾸기
+           tl_nav_show.reverse().timeScale(1.8); //애니메이션 실행
+           setTimeout(function(){
+               $gnb.removeClass('open'); //GNB배경 숨기기
+               allowScroll();
+           },340);
+        
+           function allowScroll(){
+                $(".wrap, .menu, .footer").removeAttr("style");
+                $("html").removeClass("no_scroll");
+           }
+    }
     
     function initEvent(){
+        
+        //타임라인 애니메이션 (GSAP)
+        timeline_nav();
+        
         //메뉴 클릭 시 헤더 오픈/클로즈
-        $menu.on('click',function(){
-            $gnb.toggleClass('open');
-            tl_nav_show.reversed() ? tl_nav_show.play() : tl_nav_show.pause(0).reversed(true);
+        $burger.on('click',function(evt){
+           evt.preventDefault();
+            
+           tl_nav_show.reversed() ? openBurgerMenu() : closeBurgerMenu();
+            
+           if(!$gnb.hasClass('open')){
+               $burger.removeClass('open');
+           }
         });
+        
     }
     
     
@@ -62,7 +117,6 @@ $(function(){
     $(window).on('load',function(){
         init();
         initEvent();
-        timeline_nav();
             
     });
     
